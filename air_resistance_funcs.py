e545926e539f0297ad4cbafd3253adfd9f4b70da
johncmerfeld <john.merfeld@gmail.com>
2015-04-27 12:39:39 -0400

    solved version fully functional

--- air_resistance_funcs.py
+++ air_resistance_funcs.py
@@ -13,9 +13,8 @@
 #
 
 # IMPORTED PACKAGES #####################
-from visual import *                # include standard visual python
-from visual.graph import *          # include graphing features
 from __future__ import division     # include decimal division
+from visual import *                # include standard visual python
 from numpy import pi                # include machine-precise value of pi
 
 
@@ -23,7 +22,7 @@ from numpy import pi                # include machine-precise value of pi
 
 # add appropriate values here 
 # (some of which are known constants!)
-g = 9.81                               # acceleration due to gravity in m/s^2
+g = 9.81                               # acceleration due to g in m/s^2
 rest_coeff = .9                      # percentage of velocity remaining
                                     #     after collision with ground
 p = 1.2                               # air density in kg/m^2
@@ -38,42 +37,55 @@ drag_2 = 0
 
 def initialize(name):
 
+    # both spheres should have identitcal initial conditions
     body = sphere(pos = (1,5,1), radius = 1)
-    
+    body.velocity = vector(3,20,0)      # starting velocity in m/s
+    body.accel = vector(0,0,0)          # starig acceleration in m/s^2
+    body.mass = 2.0                     # mass in kg
+
+    # but they should have different appearance and drag behavior
+    if name == 1:
+        body.color = color.yellow       # color
+        body.b = .1                     # drag coefficient for object
+    if name == 2:
+        body.color = color.red          # color
+        body.b = 0                      # drag coefficient
+
+    return body    
 
 def drag_equation(b, p, v, r):
     
     A = pi*(r**2)
-    F_drag = vector(0,0,0)            # these three could be done in one line
-    F_drag.x = -0.5*b*p*A*((v.x)**2)  # but it would be huge and gross
+    F_drag = vector(0,0,0)              # these three could be done in one line
+    F_drag.x = -0.5*b*p*A*((v.x)**2)    # but it would be huge and gross
     F_drag.y = -0.5*b*p*A*((v.y)**2)
 
     return F_drag
 
-def calculate_forces(body, p):
+def calculate_forces(body):
 
-    body.F_gravity = vector(0, -body.mass*gravity,0) # gravitational force
+    body.F_g = vector(0, -body.mass*g,0) # gravitational force
     body.F_drag = drag_equation(body.b, p, body.velocity, body.radius)
-    body.F_net = body.F_gravity + body.F_drag        # sum of forces
+    body.F_net = body.F_g + body.F_drag        # sum of forces
 
     return body
 
 
-def update_kinematics(body):
+def update_kinematics(body, delta_t):
 
     # Newton's 2nd Law
     body.accel = body.F_net / body.mass
 
     #update kinematics
-    body.velocity = body.velocity + body.accel*deltat # delta-v = a * delta-t
-    body.pos = body.pos + body.velocity*deltat        # delta-x = a * delta-a
+    body.velocity = body.velocity + body.accel*delta_t # delta-v = a * delta-t
+    body.pos = body.pos + body.velocity*delta_t        # delta-x = a * delta-a
 
     return body
 
-def collision_check(body, ground, coeff_rest):
+def collision_check(body, ground):
 
     if body.pos.y - body.radius < ground.pos.y:
-        body.velocity.y = -1 * coeff_rest * body.velocity.y
+        body.velocity.y = -1 * rest_coeff * body.velocity.y
         body.pos.y = body.radius #avoid getting trapped
 
     return body
--- air_resistance_main.py
+++ air_resistance_main.py
@@ -18,7 +18,14 @@
 
 # INCLUDE THE CODE YOU WILL WRITE #######
 
-import air_resistance_funcs
+#import air_resistance_funcs
+from air_resistance_funcs import initialize
+from air_resistance_funcs import drag_equation
+from air_resistance_funcs import calculate_forces
+from air_resistance_funcs import update_kinematics
+from air_resistance_funcs import collision_check
+from visual import *                # include standard visual python
+from visual.graph import *          # include graphing features
 
 
 # GRPAH AND VISUALIZATION SETUP #########
@@ -26,11 +33,11 @@ import air_resistance_funcs
 # graphs:
 graph_1 = gdisplay(x = 0, y = 400, width = 425, height = 400, \
                  title = "YELLOW y-velocity vs. time")
-velocitycurve_1 = gcurve(display = graph.display, color = color.yellow)
+velocitycurve_1 = gcurve(display = display, color = color.yellow)
 
 graph2_2 = gdisplay(x = 0, y = 400, width = 425, height = 400, \
                  title = "RED y-velocity vs. time")
-velocitycurve_2 = gcurve(display = graph.display, color = color.red)
+velocitycurve_2 = gcurve(display = display, color = color.red)
 
 # visualize ground in the simulation:
 ground = box(pos = (0,0,0), size = (15,0.1,100), color = color.green)
@@ -44,22 +51,23 @@ ball_2 = initialize(2)
 
 # DISCRETIZE SYSTEM #####################
 
-deltat = 0.01    # time step
+delta_t = 0.01    # time step
 t_0 = 0          # starting time
 t_end = 100      # end time
 
-
 # SIMULATE THROUGH TIME #################
 
-for t in range(t_0, t_end):
+t = t_0
+
+while t < t_end:
 
     # calculate forces
-    ball_1 = calculate_forces(ball_1, p)
-    ball_2 = calculate_forces(ball_2, p)
+    ball_1 = calculate_forces(ball_1)
+    ball_2 = calculate_forces(ball_2)
 
     # respond to forces
-    ball_1 = update_kinematics(ball_1)
-    ball_2 = update_kinematics(ball_2)
+    ball_1 = update_kinematics(ball_1, delta_t)
+    ball_2 = update_kinematics(ball_2, delta_t)
 
     # check for collision with the ground
     ball_1 = collision_check(ball_1, ground)
@@ -69,9 +77,8 @@ for t in range(t_0, t_end):
     velocitycurve_1.plot(pos=(t, ball_1.velocity.y))
     velocitycurve_2.plot(pos=(t, ball_2.velocity.y))
 
-    # update time coordinate for plot
-    t = t + deltat
-    rate(100)    # rate (frequency)
+    t += delta_t
+    rate(150)    # rate (frequency)
                  # stops computation for 1 / frequency seconds
 
 
